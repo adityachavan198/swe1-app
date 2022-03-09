cf9f5c8f907de10b47099b3b97eb5fcf7b17bade
Aditya Chavan <adityachavan198@gmail.com>
2022-03-09 11:33:50 -0500

    CI changes

--- polls/models.py
+++ polls/models.py
@@ -19,6 +19,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.choice_text
