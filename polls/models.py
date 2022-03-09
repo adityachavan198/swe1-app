d6f7848b489f8c8c86f4cf713f42df582008fcba
Aditya Chavan <adityachavan198@gmail.com>
2022-03-09 11:28:07 -0500

    Changes CI

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -8,6 +11,7 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -24,6 +24,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
